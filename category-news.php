af8455e96f99771f4a26d969f5b2afb5df0f3177
Martin Krcho <martin.krcho@gmail.com>
2017-11-23 10:20:32 +0100

    Updated the news section graphics.

--- category-news.php
+++ category-news.php
@@ -12,21 +12,25 @@ get_header();
 ?>
 
 <section id="primary" class="content-area">
+
 	<div id="content" class="site-content" role="main">
+
 		<div class="post-thumbnail corner-fx corner-fx-greywhite">
+
 			<img width="1280" height="500" src="http://www.leafacademy.eu/wp-content/uploads/2015/11/Daniel_Dluhy_-_Leaf_internet_0049.jpg">
+
 			<span class="post-thumbnail-caption-wrapper">
 				<span class="post-thumbnail-caption-wrapper-tr">
 					<span class="post-thumbnail-caption-wrapper-td">
 						<strong class="post-thumbnail-caption"><?php echo single_cat_title('', false); ?></strong>
-					</span></span></span>    
+					</span>
+				</span>
+			</span>
+
 		</div>
 
-		<div class="entry-content news-items"> 
+		<div class="entry-content news-items">
 
-			<!--
-			<?php var_dump($paged); ?>
-			-->
 			<?php
 			// Start the Loop.
 			$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
@@ -40,7 +44,7 @@ get_header();
 				'category_name' => 'news',
 				'paged' => $paged
 			);
-			
+
 			$pageposts = get_posts($args);
 			if ($pageposts):
 
@@ -49,39 +53,36 @@ get_header();
 
 					setup_postdata($post);
 					?>
-					<article>
-						<div class="news-list-thumb">
-							<?php if (has_post_thumbnail()) { ?>
-								<a href="<?php the_permalink(); ?>" class="post-smallthumbnail corner-fx corner-fx-greywhite image do-bg-image">
-									<?php
-									the_post_thumbnail('large');
-									//leafacademy_post_thumbnail();
-									?>
-								</a>
-								<?php
-							}
-							?>  
-
-						</div>
-						<div class="news-list-content">
-							<h3 class="title green-text"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-							<div class="post-date"><?php echo get_the_date(get_option('date_format'), $post->ID); ?></div>
-							<div class="text formated-output">
-								<?php the_excerpt(); ?>
-								<?php
-								$buttonLink = get_post_meta($post->ID, LEAFACADEMY_POST_LINK_META_KEY, true);
-								$buttonLabel = get_post_meta($post->ID, LEAFACADEMY_POST_LINK_LABEL_META_KEY, true);
-								$buttonTarget = get_post_meta($post->ID, LEAFACADEMY_POST_LINK_TARGET_META_KEY, true);
-								?>
-								<?php if ($buttonLink) : ?>
-									<p>
-										<a class="btn green-grey" <?php if ($buttonTarget) echo 'target="_blank"' ?> href="<?php echo $buttonLink ?>"><?php echo ($buttonLabel ? $buttonLabel : $buttonLink) ?></a>
-									</p>
+					<div class="article-wrapper">
+						<article>
+							<div class="news-list-thumb">
+								<?php if (has_post_thumbnail()): ?>
+									<a href="<?php the_permalink(); ?>" class="post-smallthumbnail corner-fx corner-fx-greywhite image do-bg-image">
+										<?php the_post_thumbnail('medium'); ?>
+									</a>
 								<?php endif; ?>
 							</div>
-						</div>
-						<div class="less-clearfix"></div>
-					</article>
+
+							<div class="news-list-content">
+								<h3 class="title green-text"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+								<div class="post-date"><?php echo get_the_date(get_option('date_format'), $post->ID); ?></div>
+								<div class="text formated-output">
+									<?php the_excerpt(); ?>
+									<?php $buttonLink = get_post_meta($post->ID, LEAFACADEMY_POST_LINK_META_KEY, true); ?>
+									<?php if ($buttonLink) : ?>
+										<?php
+										$buttonLabel = get_post_meta($post->ID, LEAFACADEMY_POST_LINK_LABEL_META_KEY, true);
+										$buttonTarget = get_post_meta($post->ID, LEAFACADEMY_POST_LINK_TARGET_META_KEY, true);
+										?>
+										<p>
+											<a class="btn green-grey" <?php if ($buttonTarget) echo 'target="_blank"' ?> href="<?php echo $buttonLink ?>"><?php echo ($buttonLabel ? $buttonLabel : $buttonLink) ?></a>
+										</p>
+									<?php endif; ?>
+								</div>
+							</div>
+							<div class="less-clearfix"></div>
+						</article>
+					</div><!-- /.article-wrapper -->
 					<?php
 				endforeach;
 			endif;
@@ -91,7 +92,9 @@ get_header();
 			?>
 
 		</div><!-- .category-news -->
+
 	</div><!-- #content -->
+
 </section><!-- #primary -->
 
 <?php
